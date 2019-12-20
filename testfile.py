f2d60a3ce33b9f1e323e4a73f5c7cf4a425bbe89
shifeng <fengshichn@gmail.com>
2019-12-20 15:22:16 -0500

    test

--- testfile.py
+++ testfile.py
@@ -1,12 +1,3 @@
 import numpy as np
 from helper import tensor_prod
-
-site = u"\u25EF  "
-left_size = 5
-right_size = 5
-
-left_block = " ".join([site] * left_size)
-right_block = " ".join([site] * right_size)
-
-print(left_block + "||  " + right_block)
-print(u"\u2192")
+# This file is for draft only
